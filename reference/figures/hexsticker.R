5a3208b7e5b2e6dc55147a6cde29a3080793a1d3
Shaunson26 <shaunson26@gmail.com>
2023-01-12 15:17:22 +0000

    Deploying to gh-pages from @ Shaunson26/MoHReportR@44dd436efc1d1e08ea86e4998c1ddfe1496f0953 🚀

---
+++ reference/figures/hexsticker.R
@@ -0,0 +1,17 @@
+
+imgurl <- "logo-body.png"
+hexSticker::sticker(
+  imgurl,
+  # title
+  package = "",
+  # image
+  s_x = 1,
+  s_y = 0.9,
+  s_width = 0.8,
+  s_height = 0.8,
+  # border
+  h_size	= 1.2,
+  h_fill	= "#002664",
+  h_color = "#d7153a",
+  filename = 'logo.png'
+)
